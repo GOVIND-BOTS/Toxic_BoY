5126b9a96cbf6acf58f3b5bc8a74f8edaf637494
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-05-13 07:37:03 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -26,9 +26,9 @@ SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
 
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "乛𝘼𝙇𝙀𝙓𝘼🕊️⃝🦋⁪⁬𝙈𝙐𝙎𝙄𝘾")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝄟⃝🥀⃟🇹‌𝐎𝐗𝐈𝐂⍣⃝🇲‌𝐔𝐒𝐈𝐂⃝💘")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6174058850 5745099463").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6008136265 6020570673").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
@@ -44,8 +44,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alexa_BotUpdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Alexa_Help")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tha_govind_op")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/indian_chatting_club_offical")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
@@ -100,7 +100,7 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/d593c6064ff7657d0c714.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/b2258dbee2401c426eb3c.jpg"
 )
 
 PING_IMG_URL = getenv(
