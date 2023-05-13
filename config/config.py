fde812ce84fbefdfe68fd869f7ce669f2b79440f
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-05-13 07:38:04 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamAlexa/AlexaMusic",
+    "https://github.com/GOVIND-BOTS/Toxic_BoY",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
